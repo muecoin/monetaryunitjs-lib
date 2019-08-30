814a666ef07fa499609abb929b273a0d3a453d8c
Sotiris Blad <26393434+sotblad@users.noreply.github.com>
2019-08-30 23:18:06 +0300

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -3,12 +3,12 @@ module.exports = {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
     bech32: 'bc',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
+      public: 0x022d2533,
+      private: 0x0221312b
     },
-    pubKeyHash: 0x3F,
-    scriptHash: 0x12,
-    wif: 0xBF
+    pubKeyHash: 0x10,
+    scriptHash: 0x4c,
+    wif: 0x7e
   },
   testnet: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
